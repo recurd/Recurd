64ea9f725e099f75ab3b151b2a76093fef5e65df
quuanng <quang.pham0824@gmail.com>
2024-10-01 18:50:31 -0500

    finished top-songs route

--- src/routes/data.js
+++ src/routes/data.js
@@ -7,7 +7,7 @@ import authGate from "../authGate.js"
 const router = Router()
 
 // Will have to figure out how to get top artists from songs with multiple artists / artists in an album
-router.get('/top-artists', (req, res) => {
+router.get('/top-artists', async (req, res) => {
 
 });
 
@@ -56,9 +56,49 @@ router.get('/top-albums', async (req, res, next) => {
     }
 });
 
+// Get song id, song name, album name, album image, artist name
+router.get('/top-songs', async (req, res) => {
+    const { user_id, start_date, end_date, n} = req.query;
 
-router.get('/top-songs', (req, res) => {
+    if (!user_id || !start_date || !end_date || !n) {
+        return res.status(400).json({ message: "user_id, start_date, end_date, and n are required." });
+    }
+
+    try {
+        const topSongs = await sql `
+            SELECT
+                s.id as song_id,
+                s.name as song_name,
+                a.name as album_name,
+                a.image as album_image,
+                ar.name as artist_name,
+                COUNT(l.id) as listen_count
+            FROM
+                listens l
+            JOIN
+                songs s ON l.song_id = s.id
+            JOIN
+                album_songs als ON s.id = als.song_id
+            JOIN
+                albums a ON als.album_id = a.id
+            JOIN
+                artist_albums ara ON a.id = ara.album_id
+            JOIN
+                artists ar ON ara.artist_id = ar.id
+            WHERE
+                l.user_id = ${user_id}
+                AND l.time_stamp BETWEEN ${start_date} and ${end_date}
+            GROUP BY
+                s.id, s.name, a.name, a.image, ar.name
+            ORDER BY
+                listen_count DESC
+            LIMIT ${n};
+        `;
     
+        res.json(topSongs);
+    } catch (error) {
+        next(error);
+    }
 });
 
 export default router;
