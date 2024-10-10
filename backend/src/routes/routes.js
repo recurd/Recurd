694ead7a1218a49610867e0014ec2d9b603a71d0
Allen Liao <wcliaw610@gmail.com>
2024-10-09 21:59:10 -0500

    first sketch of listen route

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -5,6 +5,7 @@ import serviceRouter from "./service/service.js"
 import artistRouter from "./artist.js"
 import albumRouter from "./album.js"
 import songRouter from "./song.js"
+import listenRouter from "./listen.js"
 import userDataRouter from "./user_data.js"
 
 
@@ -16,6 +17,7 @@ router.use('/service', serviceRouter)
 router.use('/artist', artistRouter)
 router.use('/album', albumRouter)
 router.use('/song', songRouter)
+router.use('/listen', listenRouter)
 router.use('/user-data', userDataRouter)
 
 export default router
