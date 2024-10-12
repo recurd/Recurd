91d6d4f5bd0bb3784ea21349e54bd5f0ec9a2af1
Allen Liao <wcliaw610@gmail.com>
2024-10-12 11:57:12 -0500

    extract log listen code to db module, change input formats of route

--- backend/src/db/metadata.js
+++ backend/src/db/metadata.js
@@ -1,32 +1,208 @@
 import sql from "./db.js"
 
-// artists is an array, each artist must contain name field
-async function insertOrFindArtists(artists) {
-    try {
-        // create temp table with same type as artists (excluding id column)
-        await sql`
-            CREATE TEMPORARY TABLE temp_artists (LIKE artists); 
-            ALTER TABLE temp_artists DROP COLUMN id`.simple() 
-        await sql`INSERT INTO temp_artists ${sql(artists, 'name', 'image', 'description')}`
-        const res = await sql`
-            WITH existed AS (
-                SELECT *
-                FROM artists 
-                WHERE name IN (SELECT name FROM temp_artists)
-            ), inserted AS (
-                INSERT INTO artists (name, image, description)
-                SELECT *
-                FROM temp_artists ta
-                WHERE NOT EXISTS (SELECT name FROM existed WHERE name = ta.name)
-                RETURNING *
-            )
-            SELECT *, TRUE AS found FROM existed
-            UNION
-            SELECT *, FALSE AS found FROM inserted`
-        // console.log('artists created and/or found:', artists)
-        await sql`DROP TABLE temp_artists`
-        return res
-    } catch (e) { return Promise.reject(e) }
+// Param song (Required): must contain 'name' field
+// Param songArtists (Required): an array of at least one element, each artist must contain 'name' field
+// Param album (Optional): must contain 'name' field
+// Param albumArtists (Optional): an array of at least one element, each artist must contain 'name' field
+// Returned: song and album objects,
+// all objects (however nested) has 'found' field that is true iff the object was found in DB
+export async function findOrInsertSongArtistAlbum(song, songArtists, album, albumArtists) {
+    let outSong = await findOrInsertSong(song, songArtists)
+
+    let outAlbum = null
+    // If song was found (not created), fetch album by name
+    if (album && outSong.found) {
+        const [resAlbum] = await sql`
+            SELECT 
+                *,
+                true AS found
+            FROM songs s
+            JOIN album_songs abs
+            ON s.id = abs.song_id
+            JOIN albums ab
+            ON abs.album_id = ab.id
+            WHERE s.id = ${outSong.id}
+                ${album.name ? sql`AND ab.name = ${album.name}` : sql``}
+            LIMIT 1`
+            outAlbum = resAlbum
+    }
+    // If haven't found album, create one when album is specified
+    if (album && !outAlbum) {
+        outAlbum = await findOrInsertAlbum(album, albumArtists)
+
+        if (!outAlbum.found) {
+            // Insert into album_songs
+            const album_song = { 
+                album_id: outAlbum.id,
+                song_id: outSong.id
+            }
+            if (song?.disc_number) {
+                outSong.disc_number = song.disc_number
+                album_song.disc_number = song.disc_number
+            }
+            if (song?.album_position) {
+                outSong.album_position = song.album_position
+                album_song.album_position = song.album_position
+            }
+            await sql`
+                INSERT INTO album_songs ${sql(album_song)}`
+        }
+    }
+    return { song: outSong, album: outAlbum }
 }
 
-export { insertOrFindArtists }
+// Param song: must contain 'name' field
+// Param songArtists: an array of at least one element, each artist must contain 'name' field
+// Returned: song, has 'found' field that is true iff the object was found in DB
+// NOT CONCURRENTLY SAFE
+async function findOrInsertSong(song, songArtists) {
+    const artist_names = songArtists.map(e=>e.name)
+    let outSong = null
+
+    // Find song by exact match on song name and artist names
+    const [matchedSA] = await sql`
+        SELECT 
+                row_to_json(s) song,
+                json_agg(row_to_json(ar)) artists
+        FROM    songs s
+        JOIN    artist_songs ars
+        ON      s.id = ars.song_id
+        JOIN    artists ar
+        ON      ars.artist_id = ar.id
+        WHERE   s.name = ${song.name}
+        GROUP BY s.id
+        HAVING  array_agg(ar.name) @> ${artist_names}
+            AND array_agg(ar.name) <@ ${artist_names};` // set equality (of arrays)
+
+    // If a match is encountered, the song exists, so retrieve data
+    if (matchedSA) {
+        outSong = {
+            ...matchedSA.song,
+            artists: matchedSA.artists.map(e=>{ return {...e, found: true} }),
+            found: true
+        }
+    }
+    // Otherwise, insert all artists whose names are not present
+    // Then insert song, and insert into artist_songs
+    else {
+        // Insert artists
+        const artists = await findOrInsertArtists(songArtists)
+
+        // Insert song
+        const [res_song] = await sql`
+            INSERT INTO songs ${sql(song, 'name', 'image')}
+            RETURNING *`
+        outSong = { 
+            ...res_song, 
+            artists: artists,
+            found: false
+        }
+        console.log('song created with id: ', outSong.id)
+
+        // Insert into artist_songs
+        const artist_song_inserts = artists.map(e => { // build values to insert into artist songs
+            return {
+                artist_id: e.id,
+                song_id: outSong.id
+            }
+        }) 
+        await sql`INSERT INTO artist_songs ${sql(artist_song_inserts)}`
+    }
+    return outSong
+}
+
+// Param album: must contain 'name' field
+// Param albumArtists: each artist must contain 'name' field
+// Returned: album object, has 'found' field that is true iff the object was found in DB.
+// Each of album's artists has 'found' field as well
+// NOT CONCURRENTLY SAFE
+async function findOrInsertAlbum(album, albumArtists) {
+    const artist_names = albumArtists.map(e=>e.name)
+    let outAlbum = null
+
+    // Find album and artists by exact match on album name and artist names
+    const [matchedAA] = await sql`
+        SELECT 
+                row_to_json(a) album,
+                json_agg(row_to_json(ar)) artists
+        FROM    albums a
+        JOIN    artist_albums aas
+        ON      a.id = aas.album_id
+        JOIN    artists ar
+        ON      aas.artist_id = ar.id
+        WHERE   a.name = ${album.name}
+        GROUP BY a.id
+        HAVING  array_agg(ar.name) @> ${artist_names}
+            AND array_agg(ar.name) <@ ${artist_names}` // set equality (of arrays)
+
+    // If a match is encountered, the song exists, so retrieve data
+    if (matchedAA) {
+        outAlbum = { 
+            ...matchedAA.album, 
+            artists: matchedAA.artists.map(e => { return {...e, found: true} }),
+            found: true
+        }
+        console.log('found album:', outAlbum)
+    } 
+    // Otherwise, insert all artists whose names are not present
+    // Then insert album, and insert into artist_albums
+    else  {
+        // Insert artists
+        const artists = await findOrInsertArtists(albumArtists)
+
+        // Insert album
+        let albumColumns = ['name', 'image', 'release_date']
+        if (album?.album_type) albumColumns.push('album_type') 
+            // album_type has constraint: not null default, so either not in column list or inserted as not null
+        const [res_album] = await sql`
+            INSERT INTO albums ${sql(album, albumColumns)}
+            RETURNING *`
+        outAlbum = { 
+            ...res_album, 
+            artists: artists,
+            found: false
+        }
+        console.log('album created with id: ', outAlbum.id)
+
+        // Insert into artist_albums
+        const artist_album_inserts = artists.map(e => { // build values to insert into artist songs
+            return {
+                artist_id: e.id,
+                album_id: outAlbum.id
+            }
+        })
+        await sql`INSERT INTO artist_albums ${sql(artist_album_inserts)}`
+    }
+    return outAlbum
+}
+
+// Param artists: an array, each artist must contain 'name' field
+// Param albumArtists: an array of at least one element, each artist must contain 'name' field
+// Returned: artists object, each has 'found' field that is true iff the object was found in DB
+// NOT CONCURRENTLY SAFE
+async function findOrInsertArtists(artists) {
+    // create temp table with same type as artists (excluding id column)
+    await sql`
+        CREATE TEMPORARY TABLE temp_artists (LIKE artists); 
+        ALTER TABLE temp_artists DROP COLUMN id`.simple() 
+    await sql`INSERT INTO temp_artists ${sql(artists, 'name', 'image', 'description')}`
+    const res = await sql`
+        WITH existed AS (
+            SELECT *
+            FROM artists 
+            WHERE name IN (SELECT name FROM temp_artists)
+        ), inserted AS (
+            INSERT INTO artists (name, image, description)
+            SELECT *
+            FROM temp_artists ta
+            WHERE NOT EXISTS (SELECT name FROM existed WHERE name = ta.name)
+            RETURNING *
+        )
+        SELECT *, TRUE AS found FROM existed
+        UNION
+        SELECT *, FALSE AS found FROM inserted`
+    // console.log('artists created and/or found:', artists)
+    await sql`DROP TABLE temp_artists`
+    return res
+}
+
--- backend/src/routes/listen.js
+++ backend/src/routes/listen.js
@@ -2,137 +2,94 @@ import { Router } from "express"
 import sql from '../db/db.js'
 import authGate from "../authGate.js"
 import { isDBError } from "../db/util.js"
-import { insertOrFindArtists } from "../db/metadata.js"
+import { findOrInsertSongArtistAlbum } from "../db/metadata.js"
 
 const router = Router()
 
+// Assumes artists is an array and non-empty
+function validArtistsArray(artists) {
+    // check if it is of string or object type
+    const type = typeof artists[0]
+    if (type !== 'string' && type !== 'object') return false
+    for (let i = 1; i < artists.length; i++) {
+        // Check if type is expected
+        if (typeof artists[i] !== type) return false
+        // Check if name exists if it is object type
+        if (type === 'object' && !artists[i]?.name) return false
+    }
+    return true
+}
+
+// Required: song name and artists (array of string artist names or artist objects with 'name' field) with length at least 1
 // Returns listen_id, song_id, artists (id, name, image per artist), album (id, name, and image)
 router.post('/log', authGate(), async (req, res, next) => {
     const { 
         song_name, 
-        artist_names
+        artists
     } = req.body
-    if (!song_name || !artist_names || 
-        !Array.isArray(artist_names) || artist_names.length == 0) {
-        res.status(400).json({ message: "song_name and artist_names (an array of at least one name) is required!" })
+    const user_id = req.session.user.id // derived from login
+    if (!song_name || !artists || 
+        !Array.isArray(artists) || artists.length == 0) {
+        res.status(400).json({ message: "song_name and artists (an array of at least one) is required!" })
         return
     }
-    let { artist_metadatas } = req.body
-    if (!artist_metadatas) artist_metadatas = artist_names.map(_=>{ return {}})
-    if (!Array.isArray(artist_metadatas) || artist_names.length != artist_metadatas.length) {
-        res.status(400).json({ message: "artist_metadatas' length must be the same as artist_names' length (if present)" })
+    if (!validArtistsArray(artists)) {
+        res.status(400).json({ message: "artists must either be an array of string or an array of json objects (with 'name' field for each artist)" })
         return
     }
 
     // optional fields
     const {
-        album_name,
         song_metadata,
-        album_metadata,
-        time_stamp = Date.now() / 1000
+        album,
+        time_stamp
     } = req.body
-    const user_id = req.session.user.id // derived from login
-
-    // Check if length of artist_metadatas match artist_names
-    if (artist_metadatas && artist_metadatas.length != artist_names.length) {
-        res.status(400).json({ "message": "artist_metadatas's length must match artist_names if it is not null" })
+    if (time_stamp && !Number.isInteger(time_stamp)) {
+        res.status(400).json({ "message": "time_stamp must be an integer (epoch time in seconds)" })
+        return
+    }
+    const albumIn = typeof album === 'string' ? { name: album } : album // possible null/undefined
+    if (albumIn?.artists && !Array.isArray(albumIn?.artists)) {
+        res.status(400).json({ "message": "album's artists must be an array"})
+        return
+    }
+    if (albumIn?.artists && albumIn.artists.length !== 0 &&
+        !validArtistsArray(albumIn.artists)) {
+        res.status(400).json({ "message": "album's artists array is not valid"})
         return
     }
 
     // See Planning doc
-    // WARNING: This feature is not concurrently safe at all!! (yet)
     try {
         const result = await sql.begin(async sql => {
-            let song = null
-            let artists = []
-            let album = null
+            // Convert artists from string into object array, if necessary
+            const songArtists = artists.map(e => {
+                if (typeof e === 'string') 
+                    return { name: e }
+                return e
+            })
+            console.log(songArtists)
 
-            // Find song and artists by exact match on song name and artist names
-            const [matchedSA] = await sql`
-                SELECT 
-                        row_to_json(s) song,
-                        json_agg(row_to_json(ar)) artists
-                FROM    songs s
-                JOIN    artist_songs ars
-                ON      s.id = ars.song_id
-                JOIN    artists ar
-                ON      ars.artist_id = ar.id
-                WHERE   s.name = ${song_name}
-                GROUP BY s.id
-                HAVING  array_agg(ar.name) @> ${artist_names}
-                    AND array_agg(ar.name) <@ ${artist_names};` // set equality (of arrays)
+            const albumArtists = (!albumIn?.artists || albumIn?.artists?.length == 0) ? songArtists : albumIn?.artists
 
-            // If a match is encountered, the song exists, so retrieve data
-            if (matchedSA) {
-                song = matchedSA.song
-                artists = matchedSA.artists
-                song.found = true
-                artists = artists.map(e=>{ return {...e, found: true} })
-            }
-            // Otherwise, insert all artists whose names are not present
-            // Then insert song, and insert into artist_songs
-            else {
-                // Insert artists
-                for (let i = 0; i < artist_names.length; i++) {
-                    artist_metadatas[i].name = artist_names[i]
-                }
-                artists = await insertOrFindArtists(artist_metadatas)
-
-                // Insert song
-                const [res_song] = await sql`
-                    INSERT INTO songs ${sql({ ...song_metadata, name: song_name})}
-                    RETURNING *`
-                song = { ...res_song, found: false }
-                console.log('song created with id: ', song.id)
-
-                // Insert artist_song entry
-                const artist_song_inserts = artists.map(e => { // build values to insert into artist songs
-                    return {
-                        artist_id: e.id,
-                        song_id: song.id
-                    }
-                }) 
-                await sql`INSERT INTO artist_songs ${sql(artist_song_inserts)}`
-            }
-
+            const { song, album } = await findOrInsertSongArtistAlbum(
+                    { ...song_metadata, name: song_name } , 
+                    songArtists, 
+                    albumIn, 
+                    albumArtists)
             // Insert listen
+            const listenInsert = { user_id, song_id: song.id }
+            if (time_stamp) listenInsert.timestamp = time_stamp
             const [{ listen_id, res_timestamp }] = await sql`
-                INSERT INTO listens ${sql( 
-                    {time_stamp: time_stamp ?? undefined, 
-                    user_id, song_id: song.id })}
-                RETURNING id, EXTRACT (EPOCH FROM time_stamp)`
-
-            // If song was found (not created), fetch album by name
-            if (song.found) {
-                const [resAlbum] = await sql`
-                    SELECT *
-                    FROM songs s
-                    JOIN album_songs abs
-                    ON s.id = abs.song_id
-                    JOIN albums ab
-                    ON abs.album_id = ab.id
-                    WHERE s.id = ${song.id}
-                        ${album_name ? sql`AND ab.name = ${album_name}` : sql``}
-                    LIMIT 1`
-                album = resAlbum
-            }
-
-            // Find album
-            if (album_name && !album) { // haven't found album
-                // Get album's artists names (prioritizing album's metadata info, but if not exists, use song's artist ames)
-                const album_artist_names = 
-                    album_metadata?.artists ?
-                    album_metadata.artists.map(e=>e.name) : 
-                        artist_names
-                
-            }
+                INSERT INTO listens ${sql(listenInsert)}
+                RETURNING id, (EXTRACT (EPOCH FROM time_stamp)::integer) as res_timestamp`
 
             return {
                 listen_id,
-                song, 
-                artists,
+                song,
+                artists: song.artists,
                 album,
-                res_timestamp
+                time_stamp: res_timestamp
             }
         })
 
