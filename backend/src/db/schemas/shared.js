807a46f8259afccdd834b96dec010791534264a1
Allen Liao <wcliaw610@gmail.com>
2024-10-13 15:15:55 -0500

    fix coerceNum schema

--- backend/src/db/schemas/shared.js
+++ backend/src/db/schemas/shared.js
@@ -4,7 +4,8 @@ import { z } from "zod"
 export const coerceStrSchemaT = z.string().or(z.number()).pipe(z.coerce.string())
 
 // Coerce only string to number
-export const coerceNumSchemaT = z.string().or(z.number()).pipe(z.coerce.number())
+// Prevent empty string from being coerced to 0
+export const coerceNumSchemaT = nonEmptyStrSchema.or(z.number()).pipe(z.coerce.number())
 
 export const nonEmptyStrSchema = z.string().min(1)
 
@@ -15,5 +16,5 @@ export const urlSchema = nonEmptyStrSchema.url()
 export const timestampSchemaT = z.union(
     [
         coerceNumSchemaT.pipe(z.number().int()), // epoch time in milliseconds
-        z.string().datetime({ message: "Datetime string must be in ISO 8601 format and in UTC timezone" })
+        z.string().datetime({ message: "Timestamp must either be a string in ISO 8601 format (with UTC timezone), or an integer representing epoch time (in milliseconds)." })
     ]).pipe(z.coerce.date()) // coerce to Date object
