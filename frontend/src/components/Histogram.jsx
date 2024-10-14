7c6a3e1938097ea9d6128ea0835d03f51d580b77
Thomas <popfallz@gmail.com>
2024-10-14 18:57:07 -0500

    Style updates

--- frontend/src/components/Histogram.jsx
+++ frontend/src/components/Histogram.jsx
@@ -10,14 +10,17 @@ const SimpleHistogram = () => {
         { rating: "3 ★, ", ratings: 8 },
         { rating: "4 ★, ", ratings: 7 },
         { rating: "5 ★, ", ratings: 1 },
+        { rating: "6 ★, ", ratings: 5 },
+        { rating: "7 ★, ", ratings: 6 },
+        { rating: "8 ★, ", ratings: 8 },
+        { rating: "9 ★, ", ratings: 7 },
+        { rating: "10 ★, ", ratings: 1 },
     ];
 
     return (
         <div style={{ width: "25%", height: "100px" }}>
             <ResponsiveContainer>
                 <BarChart data={data}>
-                    <XAxis dataKey="rating" />
-                    <YAxis />
                     <Bar dataKey="ratings" fill="#007BFF" />
                 </BarChart>
             </ResponsiveContainer>
