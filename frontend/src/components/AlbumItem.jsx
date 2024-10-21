f6821209e4a5b38f81c5aed5084cf0da54fd96c1
hoang363 <hoang363@umn.edu>
2024-10-20 22:41:19 -0500

    1st

---
+++ frontend/src/components/AlbumItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export default function AlbumItem({album}){
+    return (
+        <div>
+            <img src={album.image} alt={album.title} />
+            <p>{album.title}</p>
+            <p>{album.artist}</p>
+            <p>Rank: {album.rank}</p>
+        </div>
+    );
+}
