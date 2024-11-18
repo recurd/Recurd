1c181a2300ffa0739a0aad3467f4636ba1f2f0a4
Allen Liao <wcliaw610@gmail.com>
2024-11-18 11:28:01 -0600

    publisher rabbitmq stream

--- external-updater/src/SpotifyCron.ts
+++ external-updater/src/SpotifyCron.ts
@@ -1,7 +1,10 @@
 // import { timeout } from 'cron'
 import Database from './Database.js'
+import { getStreamClient, getStreamPublisherRef } from './Stream.js'
+import { StreamTypes } from './StreamTypes.js'
 import External from 'recurd-external'
 import dotenv from 'dotenv'
+import { ServiceType } from '../../external/dist/src/services/Types.js'
 dotenv.config()
 
 export const cronTime = '0 0 */1 * * *' // every hour
@@ -9,6 +12,14 @@ export const cronTime = '0 0 */1 * * *' // every hour
 export async function SpotifyUpdater() {
     console.log(new Date().toUTCString() + ": Starting Spotify updater...")
     // console.log(`Running again at (roughly) ${new Date(Date.now()+timeout(cronTime)).toUTCString()}`)
+
+    const streamClient = await getStreamClient(StreamTypes.LISTENS)
+    const streamPublisher = await streamClient.declarePublisher({
+            stream: StreamTypes.LISTENS,
+            publisherRef: getStreamPublisherRef(ServiceType.SPOTIFY),
+        },
+        (msg) => msg!['user_id'] // function to extract filter
+    )
 
     // Fetch a list of users' services that connected to Spotify 
     await Database.UserService.getUserIdsByType(External.ServiceType.SPOTIFY, async batch => {
@@ -18,8 +29,20 @@ export async function SpotifyUpdater() {
                 console.error(`recurd-external cannot find service ${External.ServiceType.SPOTIFY, user_id} for a user when it should exist`)
                 continue
             }
+
             const listens = await service.getRecentListens() // takes care of adding to DB
+
+            const listenMsgs = listens.map(l => {
+                return {
+                    content: Buffer.from(JSON.stringify(l)),
+                    applicationProperties: l.user_id
+                }
+            })
+            if (listens.length > 0) streamPublisher.sendSubEntries(listenMsgs)
+
             console.log(`Added ${listens.length} listens for user ${user_id}`)
         }
     })
+
+    await streamClient.close()
 }
---
+++ external-updater/src/Stream.ts
@@ -0,0 +1,33 @@
+import rabbit from 'rabbitmq-stream-js-client'
+import dotenv from "dotenv"
+dotenv.config()
+
+export async function getStreamClient(streamName : string) : Promise<rabbit.Client> {
+    const client = await rabbit.connect({
+        hostname: process.env.RABBIT_HOST!,
+        port: Number.parseInt(process.env.RABBIT_PORT!),
+        username: process.env.RABBIT_USERNAME!,
+        password: process.env.RABBIT_PASSWORD!,
+        vhost: "/",
+        // ssl: {
+        //     key: "<client_private_key>",
+        //     cert: "<client_certificate>",
+        //     ca: "<ca>", // Optional
+        // },
+    })
+
+    const streamSizeRetention = 5 * 1e9
+    await client.createStream({
+        stream: streamName,
+        arguments: {
+            'max-length-bytes': streamSizeRetention,
+            'max-age': '1D' // any message in stream has an age of 1 day
+        }
+    })
+
+    return client
+}
+
+export function getStreamPublisherRef(type : string) : string {
+    return `${type}-publisher-${new Date().toISOString()}`
+}
