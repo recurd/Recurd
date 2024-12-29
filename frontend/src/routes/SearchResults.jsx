a4f75af348b0375ca6fa0cefd8dc688a2b5a38c6
Tony Rutherford <ar56058@gmail.com>
2024-12-29 12:51:42 -0600

    Made search results links

--- frontend/src/routes/SearchResults.jsx
+++ frontend/src/routes/SearchResults.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import backend from '../backend.js';
 import { useState, useEffect } from "react";
 
@@ -54,18 +54,27 @@ export default function SearchResults() {
             <div className={"flex flex-col w-[800px] h-[100vh] px-5  py-2 am-auto overflow-y-auto border-l border-r border-gray-300"}>
                 <h2 className={"text-[150%]"}>Search Results for "{query}"</h2>
                 <h1 className={"ml-5 text-[120%]"}>Albums</h1>
-                <div className={"ml-10 text-[110%]"}>{ albumResults.length > 0 ? albumResults.map(a =>
-                    <p>{a}</p>
+                <div className={"pl-10 text-[110%] w-full border-b"}>{ albumResults.length > 0 ? albumResults.map(a =>
+                    <div>
+                    <Link to={`/album/${a.id}`} className={"transition duration-[200] hover:opacity-70"}>{a.name}</Link>
+                    <br/>
+                    </div>
                 ) : "No albums found"}
                 </div>
                 <h1 className={"ml-5 text-[120%]"}>Artists</h1>
-                <div className={"ml-10 text-[110%]"}>{ artistResults.length > 0 ? artistResults.map(a =>
-                    <p>{a}</p>
+                <div className={"pl-10 text-[110%] w-full border-b"}>{ artistResults.length > 0 ? artistResults.map(a =>
+                    <div>
+                    <Link to={`/artist/${a.id}`} className={"transition duration-[200] hover:opacity-70"}>{a.name}</Link>
+                    <br/>
+                    </div>
                 ) : "No artists found"}
                 </div>
                 <h1 className={"ml-5 text-[120%]"}>Songs</h1>
-                <div className={"ml-10 text-[110%]"}>{ songResults.length > 0 ? songResults.map(a =>
-                    <p>{a}</p>
+                <div className={"pl-10 text-[110%] w-full border-b"}>{ songResults.length > 0 ? songResults.map(a =>
+                    <div>
+                    <Link to={`/song/${a.id}`} className={"transition duration-[200] hover:opacity-70"}>{a.name}</Link>
+                    <br/>
+                    </div>
                 ) : "No songs found"}
                 </div>
             </div>
